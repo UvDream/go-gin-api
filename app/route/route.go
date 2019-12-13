9720d682f84797b274a80bddb62d2deb1ad2ef0b
wangzhongjie <UvDream@163.com>
2019-12-13 14:28:32 +0800

    feat:tada:404方法

--- app/route/route.go
+++ app/route/route.go
@@ -1,29 +1,37 @@
 package route
 
 import (
-	"github.com/gin-gonic/gin"
 	"go-gin-api/app/controller/product"
 	"go-gin-api/app/util"
+
+	"github.com/gin-gonic/gin"
 )
 
+// SetupRouter 路由
 func SetupRouter(engine *gin.Engine) {
+	// 测试路由
 	engine.GET("/ping", func(c *gin.Context) {
-		utilGin := util.Gin{Ctx:c}
-		utilGin.Response(1,"pong", nil)
+		utilGin := util.Gin{Ctx: c}
+		utilGin.Response(1, "pong", nil)
+	})
+	// 404
+	engine.NoRoute(func(c *gin.Context) {
+		utilGin := util.Gin{Ctx: c}
+		utilGin.Response(404, "请求方法不存在", nil)
 	})
 
-	ProductRouter := engine.Group("")
+	ProductRouter := engine.Group("/product")
 	{
 		// 新增产品
-		ProductRouter.POST("/product", product.Add)
+		ProductRouter.POST("", product.Add)
 
 		// 更新产品
-		ProductRouter.PUT("/product/:id", product.Edit)
+		ProductRouter.PUT("/:id", product.Edit)
 
 		// 删除产品
-		ProductRouter.DELETE("/product/:id", product.Delete)
+		ProductRouter.DELETE("/:id", product.Delete)
 
 		// 获取产品详情
-		ProductRouter.GET("/product/:id", product.Detail)
+		ProductRouter.GET("/:id", product.Detail)
 	}
 }
