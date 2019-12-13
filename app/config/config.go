f494f1d0686f505066907b2eb1f1e0b3cacfa7f0
wangzhongjie <UvDream@163.com>
2019-12-13 09:41:22 +0800

    feat:tada: 设置路由

---
+++ app/config/config.go
@@ -0,0 +1,8 @@
+package config
+
+const (
+	// AppPort 端口号
+	AppPort = ":8000"
+	// AppName 名字
+	AppName = "go-gin-api"
+)
--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"go-gin-api/app/route"
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run()
+	// 设置gin模式
+	// 1.DebugMode
+	// 2.ReleaseMode
+	// 3.TestMode
+	gin.SetMode(gin.DebugMode)
+	engine := gin.New()
+	// 设置路由
+	route.SetupRouter(engine)
+
+	// 启动服务
+	if err := engine.Run(":9000"); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	// r := gin.Default()
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+	// r.Run()
 
 }
